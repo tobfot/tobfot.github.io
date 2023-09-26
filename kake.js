45dea8e2a6f307bc3f2456ad58f156a44cc17af3
tobfot <146085962+tobfot@users.noreply.github.com>
2023-09-26 13:14:14 +0200

    Add files via upload

---
+++ kake.js
@@ -0,0 +1,79 @@
+const lyd = new Audio("pop.mp3");
+
+let tidIgjen = 5;
+let tidsTimer = setInterval(tidstalking, 1000);
+function tidtracking() {
+    if(tidIgjen <= 0) {
+        
+    }
+}
+
+function stopp() {
+    //alert(stopp) 
+    alert("STOPP!")
+    poengsum = 0;
+    document.getElementById("kake").removeEventListener("click", klikk);
+}
+
+
+
+// henter ut kake-bildet og legger til en lyttefunksion som kjører når du klikker
+
+document.getElementById("kake").addEventListener("click",klikk)
+
+let poengsum = 0; 
+
+let stor = true;
+
+function klikk() {
+    lyd.play();
+    poengsum = poengsum + 1;
+
+    document.getElementById("poeng").innerText = poengsum;
+
+    if (stor) {
+        document.getElementById("kake").style.transform = "scale(0.8)";
+        stor=false;
+    } else {
+        document.getElementById("kake").style.transform = "scale(1)";
+        stor=true;
+    }
+
+    if (poengsum > 9) {
+        document.getElementById("kake").src = "R1.png";
+        // document.getElementById("kake").width = "400";
+    }
+    
+    if(poengsum > 19){
+        document.getElementById("kake").src = "R2.png";
+    }
+
+    if(poengsum > 29){
+        document.getElementById("kake").src = "R3.png";
+    }
+
+    if(poengsum > 39){
+        document.getElementById("kake").src = "R4.png";
+    }
+
+    if(poengsum > 49){
+        document.getElementById("kake").src = "R5.png";
+    }
+
+    if(poengsum > 59){
+        document.getElementById("kake").src = "R6.png";
+    }
+
+}
+
+
+document.getElementById("knap").addEventListener("click",restart)
+
+function restart(){
+    const timeout = setTimeout(stopp, 2000);
+    poengsum = 0;
+
+    document.getElementById("poeng").innerText = poengsum;
+
+    document.getElementById("kake").src = "R0.png";
+}
